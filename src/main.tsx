1d9bdca7e1d37b2cb85e62062de492073843494c
KishanthDev <kishanthkisna@gmail.com>
2026-04-03 10:48:23 +0530

    template

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import Dashboard from './Dashboard.tsx'
+import "./index.css"
+import MfeDashboard from './MfeDashboard'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <Dashboard />
+    <MfeDashboard/>
   </StrictMode>,
 )
